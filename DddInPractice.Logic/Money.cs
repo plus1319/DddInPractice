bc39312a528ba05dc1f842c0754ad4cf8ac5f15a
Amir Sh <plus1319@outlook.com>
2020-12-24 22:50:09 +0330

    add uinit testing and refactor ...

--- DddInPractice.Logic/Money.cs
+++ DddInPractice.Logic/Money.cs
@@ -1,16 +1,47 @@
-﻿namespace DddInPractice.Logic
+﻿using System;
+
+namespace DddInPractice.Logic
 {
     public class Money : ValueObject<Money>
     {
-        public int OneCentCount { get; private set; }
-        public int TenCentCount { get; private set; }
-        public int QuarterCount { get; private set; }
-        public int OneDollarCount { get; private set; }
-        public int FiveDollarCount { get; private set; }
-        public int TwentyDollarCount { get; private set; }
+        public static readonly Money None = new Money(0,0,0,0,0,0);
+        public static readonly Money Cent = new Money(1,0,0,0,0,0);
+        public static readonly Money TenCent = new Money(0,1,0,0,0,0);
+        public static readonly Money Quarter = new Money(0,0,1,0,0,0);
+        public static readonly Money Dollar = new Money(0,0,0,1,0,0);
+        public static readonly Money FiveDollar = new Money(0,0,0,0,1,0);
+        public static readonly Money TwentyDollar = new Money(0,0,0,0,0,1);
+
+        public int OneCentCount { get;}
+        public int TenCentCount { get; }
+        public int QuarterCount { get;}
+        public int OneDollarCount { get;}
+        public int FiveDollarCount { get;}
+        public int TwentyDollarCount { get;}
+
+        public decimal Amount =>
+            OneCentCount * 0.01m +
+            TenCentCount * 0.10m +
+            QuarterCount * 0.25m +
+            OneDollarCount +
+            FiveDollarCount * 5 +
+            TwentyDollarCount * 20;
 
         public Money(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
         {
+            if(oneCentCount < 0)
+                throw new InvalidOperationException();
+            if(tenCentCount < 0)
+                throw new InvalidOperationException();
+            if(quarterCount < 0)
+                throw new InvalidOperationException();
+            if(oneDollarCount < 0)
+                throw new InvalidOperationException();
+            if(fiveDollarCount < 0)
+                throw new InvalidOperationException();
+            if(twentyDollarCount < 0)
+                throw new InvalidOperationException();
+
             OneCentCount = oneCentCount;
             TenCentCount = tenCentCount;
             QuarterCount = quarterCount;
@@ -33,6 +64,18 @@
             return sum;
         }
 
+        public static Money operator -(Money money1, Money money2)
+        {
+            return new Money(
+                money1.OneCentCount - money2.OneCentCount,
+                money1.TenCentCount - money2.TenCentCount,
+                money1.QuarterCount - money2.QuarterCount,
+                money1.OneDollarCount - money2.OneDollarCount,
+                money1.FiveDollarCount - money2.FiveDollarCount,
+                money1.TwentyDollarCount - money2.TwentyDollarCount
+            );
+        }
+      
         protected override bool EqualsCore(Money other)
         {
             return OneCentCount == other.OneCentCount
@@ -56,5 +99,7 @@
                 return hashCode;
             }
         }
+
+
     }
 }
---
+++ DddInPractice.Tests/SnackMachineSpecs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DddInPractice.Logic;
+using FluentAssertions;
+using Xunit;
+
+using static DddInPractice.Logic.Money;
+
+namespace DddInPractice.Tests
+{
+    public class SnackMachineSpecs
+    {
+        [Fact]
+        public void return_money_empties_money_in_transaction()
+        {
+            var snackMachine = new SnackMachine();
+            snackMachine.InsertMoney(Dollar);
+
+            snackMachine.ReturnMoney();
+
+            snackMachine.MoneyInTransaction.Amount.Should().Be(0m);
+        }
+
+        [Fact]
+        public void inserted_money_goes_to_money_in_transaction()
+        {
+            var snackMachine = new SnackMachine();
+
+            snackMachine.InsertMoney(Cent);
+            snackMachine.InsertMoney(Dollar);
+
+            snackMachine.MoneyInTransaction.Amount.Should().Be(1.01m);
+        }
+
+        [Fact]
+        public void cannot_insert_more_than_one_coin_or_note_at_a_time()
+        {
+            var snackMachine = new SnackMachine();
+            var twoCents = Cent + Cent;
+
+            Action action = () => snackMachine.InsertMoney(twoCents);
+            
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void money_in_transaction_goes_to_money_inside_after_purchase()
+        {
+            var snackMachine = new SnackMachine();
+            snackMachine.InsertMoney(Dollar);
+            snackMachine.InsertMoney(Dollar);
+
+            snackMachine.BuySnack();
+
+            snackMachine.MoneyInTransaction.Amount.Should().Be(0m);
+            snackMachine.MoneyInside.Amount.Should().Be(2m);
+        }
+    }
+}
